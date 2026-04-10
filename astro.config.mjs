d1674872e6ad6fc0cae92f1b627a2d38ef7c4062
hachisnail <jeffersontalagtag06@gmail.com>
2026-04-10 10:02:18 +0800

    added site and base to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://hachisnail.github.io/ao-loot-comparator/",
+  base: "/ao-loot-comparator",
   vite: {
     plugins: [tailwindcss()],
   },
-
-  integrations: [react()],
 });
