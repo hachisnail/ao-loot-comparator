8c635a7d9d66a7385aa309303064a1494cb42402
hachisnail <jeffersontalagtag06@gmail.com>
2026-04-10 10:26:21 +0800

    added react to integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://hachisnail.github.io",
   base: "/ao-loot-comparator",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [react()],
 });
